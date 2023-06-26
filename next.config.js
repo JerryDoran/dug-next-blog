3b0b9131ca2a18d5a9aba1122764e924baca12c9
Jerome Doran <disneyfan9184@gmail.com>
2023-06-26 15:04:08 -0400

    added site map and custom image and video components

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["encrypted-tbn0.gstatic.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/JerryDoran/remote-mdx-blog-manager/main/images/**',
+      },
+    ],
+    domains: ['encrypted-tbn0.gstatic.com'],
   },
 };
 
